25d8f3bf34a6200236f9c4af3abd7486fe8837c8
unknown <yyk7935@gmail.com>
2021-02-16 13:29:28 +0900

    로그인 기능 까지만

---
+++ src/aaa10/AClass.java
@@ -0,0 +1,7 @@
+package aaa10;
+
+public class AClass {
+	public AClass() {
+		System.out.println("a가 만든 기능");
+	}
+}
--- src/aaa10/MainClass.java
+++ src/aaa10/MainClass.java
@@ -1,5 +1,7 @@
 package aaa10;
 
 public class MainClass {
-
+	public static void main(String[] args) {
+		System.out.println("공통 내용 틀 입");
+	}
 }
